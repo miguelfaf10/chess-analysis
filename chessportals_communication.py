9ce1e256d7c1ee7aa93b7201b2793efe6aa5a174
Miguel Fernandes <miguelfaf10@gmail.com>
2022-07-23 04:05:21 +0200

    Create modules data_structures and chessportals_communication

---
+++ chessportals_communication.py
@@ -0,0 +1,149 @@
+from typing import List
+from datetime import datetime
+import pandas as pd
+import berserk
+
+from data_structures import GameModel, UserData
+
+class lichess_communication:
+    
+    lichess_id = None
+    API_TOKEN = None
+    client = None
+    
+    user_data = None
+    
+    get_opening = True
+    get_evals = True
+    games_lst = []
+    games_df = None
+    
+    def __init__(self, user) -> None:
+        
+        # Initialize token
+        with open('conf/token.txt') as f:
+            self.API_TOKEN = f.readline()[:-1]
+        
+        # Initialize lichess client
+        self.lichess_id = user
+        session = berserk.TokenSession(self.API_TOKEN)
+        self.client = berserk.Client(session=session)
+    
+    def fetch_user_rating(self):
+        
+        player_rating = self.client.users.get_rating_history(self.lichess_id)
+
+        return player_rating
+
+    def fetch_user_data(self):
+        
+        user_data = self.client.users.get_public_data(self.lichess_id)
+
+        self.user_data = UserData(**user_data)
+
+        return self.user_data
+
+    def fetch_games_by_dates(self, since: datetime, until: datetime) -> List[GameModel]:
+    
+        since_millis = int(berserk.utils.to_millis(since))
+        until_millis = int(berserk.utils.to_millis(until))
+
+        games_gen = self.client.games.export_by_player(self.lichess_id,
+                                                       rated=True,
+                                                       since=since_millis,
+                                                       until=until_millis,
+                                                       evals=self.get_evals,
+                                                       opening=self.get_opening)
+        
+        self.games_lst = [GameModel(**game) for game in games_gen] 
+
+        return self.games_lst
+    
+        #for game in games_gen:
+        #    try:
+        #        self.games_lst.append(GameModel(**game))
+        #    except:
+        #        print(game)
+        
+    def show_games_info(self) -> None:
+        
+        if self.games_lst == None:
+            print('No games have been fetched')
+            return False
+        else:
+            print(f'Fetched {len(self.games_lst)} games')
+            print(f'Last game from  : {self.games_lst[0].createdAt}')
+            print(f'First game from : {self.games_lst[-1].createdAt}')
+
+    def show_user_data(self) -> None:
+        
+        if self.user_data == None:
+            print('No user data has been fetched')
+            return False
+        else:
+            print(f'User {self.user_data.id} created at {self.user_data.createdAt}')
+            print(f'Rating Classical : {self.user_data.perfs.classical.rating:4} ({self.user_data.perfs.classical.games:4} games)')
+            print(f'Rating Rapid     : {self.user_data.perfs.rapid.rating:4} ({self.user_data.perfs.rapid.games:4} games)')
+            print(f'Rating Blitz     : {self.user_data.perfs.blitz.rating:4} ({self.user_data.perfs.blitz.games:4} games)')
+            print(f'Rating Bullet    : {self.user_data.perfs.bullet.rating:4} ({self.user_data.perfs.bullet.games:4} games)')
+
+    def fill_df(self) -> pd.DataFrame:
+
+        games_dict = dict(id =[],
+                          color = [],
+                          opponent = [],
+                          time_control = [],
+                          datetime = [],
+                          opening = [],
+                          result = [],
+                          moves = [],
+                          analysis = [],
+                          evals = [],
+                          mates = [],
+                          judgment = [])
+
+        for game in self.games_lst:
+
+            games_dict['id'].append(game.id)
+
+            games_dict['color'].append('white' if (game.players.white==self.lichess_id) else 'black')
+            
+            if game.players.white.user.id==self.lichess_id:
+                games_dict['opponent'].append(game.players.black.user.id)
+            else:
+                games_dict['opponent'].append(game.players.white.user.id)
+            
+            games_dict['datetime'].append(game.createdAt)
+            
+            games_dict['opening'].append(game.opening.name)
+            
+            if  game.winner=='draw':
+                games_dict['result'].append('draw')
+            elif game.winner=='white' and game.players.white.user.id==self.lichess_id:
+                games_dict['result'].append('win')
+            elif game.winner=='black' and game.players.black.user.id==self.lichess_id:
+                games_dict['result'].append('win')
+            else:
+                games_dict['result'].append('loss')
+
+            games_dict['time_control'].append(game.speed)
+
+           
+            games_dict['moves'].append(game.moves.split(' '))
+            
+            games_dict['analysis'].append(True if game.analysis!=None else False)
+            
+            print(game.analysis)
+            games_dict['evals'].append([move_anal.eval for move_anal in game.analysis] 
+                                       if game.analysis!=None else None)
+            
+            games_dict['mates'].append([move_anal.mate for move_anal in game.analysis] 
+                                       if game.analysis!=None else None)
+            
+            games_dict['judgment'].append([move_anal.judgment.name if move_anal.judgment!='' else '' for move_anal in game.analysis] 
+                                          if game.analysis!=None else None)
+
+        self.games_df = pd.DataFrame.from_dict(games_dict, orient='columns')
+        #self.games_df.set_index('id', inplace=True)
+        
+        return self.games_df
---
+++ data_structures.py
@@ -0,0 +1,79 @@
+from __future__ import annotations
+from typing import List, Optional
+from pydantic import BaseModel
+from datetime import datetime
+
+"""Pydantic Data Class to hold game data 
+"""
+class User(BaseModel):
+    name: str
+    id: str
+
+class GlobalAnalysis(BaseModel):
+    inaccuracy: int
+    mistake: int
+    blunder: int
+    acpl: int
+
+class Player(BaseModel):
+    user: User
+    rating: int
+    ratingDiff: Optional[int] = 0
+    analysis: Optional[GlobalAnalysis] = None
+
+class Players(BaseModel):
+    white: Player
+    black: Player
+
+class Opening(BaseModel):
+    eco: str
+    name: str
+    ply: int
+        
+class Judgment(BaseModel):
+    name: str
+    comment: str
+
+class MoveAnalysis(BaseModel):
+    mate: Optional[int] = 'nan'
+    eval: Optional[int] = 'nan'
+    best: Optional[str]
+    variation: Optional[str]
+    judgment: Optional[Judgment] = ''
+
+class Clock(BaseModel):
+    initial: int
+    increment: int
+    totalTime: int
+
+class GameModel(BaseModel):
+    id: str
+    rated: bool
+    variant: str
+    speed: str
+    createdAt: datetime
+    lastMoveAt: datetime
+    status: str
+    players: Players
+    winner: Optional[str] = 'draw'
+    opening: Optional[Opening] = None
+    moves: str
+    analysis: Optional[List[MoveAnalysis]] = None
+    clock: Clock
+
+"""Pydantic Data Class to hold user data
+"""
+class Performance(BaseModel):
+    games: int
+    rating: int
+
+class Variants(BaseModel):
+    bullet: Performance
+    blitz: Performance
+    rapid: Performance
+    classical: Performance
+
+class UserData(BaseModel):
+    id: str
+    createdAt: datetime
+    perfs: Variants
